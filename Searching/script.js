6f738643e5b0c1cc5f19c5ae080f204b0f9b87b0
Bhuvnesh Verma <99537126+MasterBhuvnesh@users.noreply.github.com>
2024-10-24 23:09:00 +0530

    Delete Searching directory

--- Searching/script.js
+++
@@ -1,7 +0,0 @@
-function navigateToPage() {
-    var selectedOption = document.getElementById("searchType").value;
-    if (selectedOption) {
-      window.location.href = selectedOption; // Redirect to the selected option's value (URL)
-    }
-  }
-  
